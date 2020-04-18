e65a42ff82a0da54a15fee3c1b2355be3c0d056f
Thomas Symborski <thomas.symborski@gmail.com>
2020-04-18 16:45:18 -0400

    jobs: add models for job status

--- transcode/dto.go
+++ transcode/dto.go
@@ -1,12 +1,17 @@
 package transcode
 
 type CreateJobRequest struct {
-	Source  CreateJobSource   `json:"source"`
-	Outputs []CreateJobOutput `json:"outputs"`
+	Source      CreateJobSource      `json:"source"`
+	Destination CreateJobDestination `json:"destination"`
+	Outputs     []CreateJobOutput    `json:"outputs"`
 }
 
 type CreateJobSource struct {
 	URL string `json:"url"`
+}
+
+type CreateJobDestination struct {
+	BaseURL string `json:"baseURL"`
 }
 
 type CreateJobOutput struct {
@@ -40,4 +45,24 @@ const (
 type AudioSource struct {
 	SourceIndex int   `json:"sourceIndex"`
 	Tracks      []int `json:"tracks"`
+}
+
+type JobStatusResponse struct {
+	ID      string            `json:"id"`
+	Status  JobStatus         `json:"status"`
+	Outputs []JobStatusOutput `json:"outputs,omitempty"`
+}
+
+type JobStatus string
+
+const (
+	JobStatusQueued   JobStatus = "queued"
+	JobStatusStarted  JobStatus = "started"
+	JobStatusFinished JobStatus = "finished"
+	JobStatusErrored  JobStatus = "errored"
+	JobStatusCanceled JobStatus = "canceled"
+)
+
+type JobStatusOutput struct {
+	URL string `json:"url"`
 }
--- transcode/service.go
+++ transcode/service.go
@@ -1,1 +1,6 @@
 package transcode
+
+type Service interface {
+	CreateJob(CreateJobRequest) (string, error)
+	JobStatus(string) (JobStatusResponse, error)
+}
